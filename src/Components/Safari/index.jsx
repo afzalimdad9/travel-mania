4e5f8170f2c9ee262a8fe1ccb5e0ae5354de29cb
qureshikaif <qureshik488@gmail.com>
2024-08-11 20:40:43 +0500

    feat: updated image banner in details page

--- src/Components/Safari/index.jsx
+++ src/Components/Safari/index.jsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import { Breadcrumb, Col, Container, Row } from "react-bootstrap";
+import Details1 from "../../../public/images/details-1.png";
+import Details2 from "../../../public/images/details-2.png";
+import Details3 from "../../../public/images/details-3.png";
+import Details4 from "../../../public/images/details-4.png";
+import Details5 from "../../../public/images/details-5.png";
 
 function SafariSection() {
   return (
@@ -8,14 +13,16 @@ function SafariSection() {
       {/* Breadcrumbs and Title */}
       <Row className="mb-3">
         <Col>
-          <Breadcrumb>
+          <Breadcrumb className="my-5">
             <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
             <Breadcrumb.Item href="#">Attractions</Breadcrumb.Item>
             <Breadcrumb.Item active>
               Dubai Desert Safari with BBQ Dinner
             </Breadcrumb.Item>
           </Breadcrumb>
-          <h1>Dubai Desert Safari with BBQ Dinner</h1>
+          <p className="text-black fs-2 fw-bold">
+            Dubai Desert Safari with BBQ Dinner
+          </p>
           <p>
             Dune bashing followed by cultural activities, live entertainment,
             and barbecue dinner
@@ -43,41 +50,55 @@ function SafariSection() {
 
       {/* Photo Gallery */}
       <Row>
-        <Col xs={12} sm={6} md={3}>
+        <Col xs={12} md={8}>
           <Image
-            src="/path-to-your-image-1.jpg"
+            src={Details1} // Large image
             alt="Dubai City"
-            width={500} // Adjust based on your layout needs
-            height={300}
+            width={800} // Adjust these sizes based on your layout needs
+            height={600}
+            className="h-75"
             layout="responsive"
           />
         </Col>
-        <Col xs={12} sm={6} md={3}>
-          <Image
-            src="/path-to-your-image-2.jpg"
-            alt="Camel Ride"
-            width={500}
-            height={300}
-            layout="responsive"
-          />
-        </Col>
-        <Col xs={12} sm={6} md={3}>
-          <Image
-            src="/path-to-your-image-3.jpg"
-            alt="Sand Dunes"
-            width={500}
-            height={300}
-            layout="responsive"
-          />
-        </Col>
-        <Col xs={12} sm={6} md={3}>
-          <Image
-            src="/path-to-your-image-4.jpg"
-            alt="Desert Safari"
-            width={500}
-            height={300}
-            layout="responsive"
-          />
+        <Col xs={12} md={4}>
+          <Row>
+            <Col xs={6} className="mb-2">
+              <Image
+                src={Details2} // Top left small image
+                alt="Camel Ride"
+                width={400}
+                height={300}
+                layout="responsive"
+              />
+            </Col>
+            <Col xs={6} className="mb-2">
+              <Image
+                src={Details3} // Top right small image
+                alt="Sand Dunes"
+                width={400}
+                height={300}
+                layout="responsive"
+              />
+            </Col>
+            <Col xs={6}>
+              <Image
+                src={Details4} // Bottom left small image
+                alt="Desert Safari"
+                width={400}
+                height={300}
+                layout="responsive"
+              />
+            </Col>
+            <Col xs={6}>
+              <Image
+                src={Details5} // Bottom right small image
+                alt="Desert Safari"
+                width={400}
+                height={300}
+                layout="responsive"
+              />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
