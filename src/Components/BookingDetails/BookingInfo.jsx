239d4374e0821927ca3cd07f59917ba634c304e8
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-01 13:47:59 +0500

    fix: booking info card according to theme

--- src/Components/BookingDetails/BookingInfo.jsx
+++ src/Components/BookingDetails/BookingInfo.jsx
@@ -10,99 +10,92 @@ const BookingInfo = ({ data }) => {
     <Container>
       <Row className="my-4">
         <Col>
-          <Card>
-            <Card.Header as="h5">Hotel Information</Card.Header>
-            <Card.Body>
-              <Card.Title>{`Hotel Code: ${hotel?.HotelCode}`}</Card.Title>
-              <Card.Text>
-                <strong>Currency:</strong> {hotel?.Currency}
-              </Card.Text>
-              <hr />
-              <Card.Title>Room Information</Card.Title>
-              <Card.Text>
-                <strong>Name:</strong> {room?.Name?.[0]}
-              </Card.Text>
-              <Card.Text>
-                <strong>Booking Code:</strong> {room?.BookingCode}
-              </Card.Text>
-              <Card.Text>
-                <strong>Inclusion:</strong> {room?.Inclusion}
-              </Card.Text>
-              <Card.Text>
-                <strong>Meal Type:</strong> {room?.MealType}
-              </Card.Text>
-              <Card.Text>
-                <strong>Total Fare:</strong> ${room?.TotalFare}
-              </Card.Text>
-              <Card.Text>
-                <strong>Total Tax:</strong> ${room?.TotalTax}
-              </Card.Text>
-              <hr />
-              <Card.Title>Cancel Policies</Card.Title>
-              <ListGroup variant="flush">
-                {(room?.CancelPolicies || [])?.map((policy, index) => (
-                  <ListGroup.Item key={index}>
-                    <strong>From Date:</strong> {policy.FromDate} <br />
-                    <strong>Charge Type:</strong> {policy.ChargeType} <br />
-                    <strong>Cancellation Charge:</strong>{" "}
-                    {policy.CancellationCharge}%
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-              <hr />
-              <Card.Title>Amenities</Card.Title>
+          <h5 className="mb-3">Room Information</h5>
+          <Card.Text>
+            <strong>Name:</strong> {room?.Name?.[0]}
+          </Card.Text>
+          <Card.Text>
+            <strong>Booking Code:</strong> {room?.BookingCode}
+          </Card.Text>
+          <Card.Text>
+            <strong>Inclusion:</strong> {room?.Inclusion}
+          </Card.Text>
+          <Card.Text>
+            <strong>Meal Type:</strong> {room?.MealType}
+          </Card.Text>
+          <Card.Text>
+            <strong>Total Fare:</strong> ${room?.TotalFare}
+          </Card.Text>
+          <Card.Text>
+            <strong>Total Tax:</strong> ${room?.TotalTax}
+          </Card.Text>
+          <hr />
+          <h5 className="mb-3">Cancel Policies</h5>
+          <ListGroup variant="flush">
+            {(room?.CancelPolicies || [])?.map((policy, index) => (
+              <ListGroup.Item key={index}>
+                <strong>From Date:</strong> {policy.FromDate} <br />
+                <strong>Charge Type:</strong> {policy.ChargeType} <br />
+                <strong>Cancellation Charge:</strong>{" "}
+                {policy.CancellationCharge}%
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <hr />
+          {(room?.Amenities || []).length > 0 && (
+            <>
+              <h5 className="mb-3">Amenities</h5>
               <ListGroup variant="flush">
                 {(room?.Amenities || []).map((amenity, index) => (
                   <ListGroup.Item key={index}>{amenity}</ListGroup.Item>
                 ))}
               </ListGroup>
-              <hr />
-              <Card.Title>Day Rates</Card.Title>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Day</th>
-                    <th>Base Price (USD)</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {room?.DayRates[0].map((rate, index) => (
-                    <tr key={index}>
-                      <td>{index + 1}</td>
-                      <td>${rate.BasePrice}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-              <hr />
-              <Card.Title>Rate Conditions</Card.Title>
-              <ListGroup variant="flush">
-                {hotel?.RateConditions.map((condition, index) => (
-                  <ListGroup.Item key={index}>{condition}</ListGroup.Item>
-                ))}
-              </ListGroup>
-              <hr />
-              <Card.Title>Credit Card Billing Options</Card.Title>
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                    <th>Convenience Charges</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {hotel?.CreditCardBillingOptions.map((option, index) => (
-                    <tr key={index}>
-                      <td>${option.Amount}</td>
-                      <td>{option.Currency}</td>
-                      <td>{option.ConvenienceCharges}%</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Card.Body>
-          </Card>
+            </>
+          )}
+          <hr />
+          <h5 className="mb-3">Day Rates</h5>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Base Price (USD)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {room?.DayRates[0].map((rate, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>${rate.BasePrice}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <hr />
+          <h5 className="mb-3">Rate Conditions</h5>
+          <ListGroup variant="flush">
+            {hotel?.RateConditions.map((condition, index) => (
+              <ListGroup.Item key={index}>{condition}</ListGroup.Item>
+            ))}
+          </ListGroup>
+          <h5 className="mb-3">Credit Card Billing Options</h5>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Amount</th>
+                <th>Currency</th>
+                <th>Convenience Charges</th>
+              </tr>
+            </thead>
+            <tbody>
+              {hotel?.CreditCardBillingOptions.map((option, index) => (
+                <tr key={index}>
+                  <td>${option.Amount}</td>
+                  <td>{option.Currency}</td>
+                  <td>{option.ConvenienceCharges}%</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </Col>
       </Row>
     </Container>
